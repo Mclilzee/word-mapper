38320d058ce97211d92cab99a91fd236f71023a6
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-14 06:25:01 +0100

    Update to only contain alphabetic and remove punctatuion set

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::path::Path;
 use std::{
     fs::{read_dir, read_to_string},
@@ -18,7 +19,7 @@ fn main() {
         .map(read_file_content)
         .collect();
 
-    print!("{content}");
+    count_words(content);
 }
 
 fn extract_files(path: PathBuf) -> Vec<PathBuf> {
@@ -45,4 +46,28 @@ fn read_file_content(path: PathBuf) -> String {
     } else {
         "".to_owned()
     }
+}
+
+fn count_words(content: String) -> String {
+    let content: String = content
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .collect();
+
+    let content: Vec<&str> = content.split(' ').collect();
+    let mut count: HashMap<String, u32> = HashMap::new();
+
+    for word in content {
+        let i = count.get(word).unwrap_or(&0);
+        count.insert(word.to_owned(), i + 1);
+    }
+
+    let mut entries: Vec<(&String, &u32)> = count.iter().collect();
+    entries.sort_by(|a, b| b.1.cmp(a.1));
+
+    entries.iter().map(|t| format!("{}: {}", t.0, t.1));
+    // .for_each(|s| print!("{s}"));
+
+    String::new()
 }
