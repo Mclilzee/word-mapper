383d36f8c6bf86c50dc2d2955a823c44426dce5d
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-14 04:18:48 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,30 @@
-use std::fs::{read_dir, read_to_string};
+use std::path::Path;
+use std::{
+    fs::{read_dir, read_to_string},
+    path::PathBuf,
+};
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
-    if args.len() <= 1 {
-        eprint!("No files to read were provided");
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    if args.is_empty() {
+        eprint!("No files were provided");
         return;
-    };
+    }
 
-    let path = match args.get(1) {
-        Some(path) => path,
-        None => {
-            eprint!("Error reading directory");
-            return;
-        }
-    };
+    let content: String = args
+        .iter()
+        .flat_map(extract_files)
+        .map(|x| read_file_content(&x))
+        .collect();
 
-    let content = read_content(path);
     print!("{content}");
 }
 
-fn read_content(path: &str) -> String {
-    if let Ok(dir) = read_dir(path) {
-        return dir
-            .filter_map(|x| x.ok())
-            .map(|x| x.path())
-            .map(read_to_string)
-            .filter_map(|x| x.ok())
-            .collect();
-    };
+fn extract_files(paths: &String) -> Vec<PathBuf> {
+    Vec::new()
+}
 
+fn read_file_content(path: &PathBuf) -> String {
     if let Ok(content) = read_to_string(path) {
         content
     } else {
