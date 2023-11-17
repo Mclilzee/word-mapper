cb86d683c4b3b5cf2dcfd50c501b0554aaa4e8f8
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-17 06:33:55 +0100

    Add printing summary

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,36 @@ fn main() {
         return;
     }
 
-    args.iter()
+    let print_summary = args.len() > 2 && args.get(1).unwrap() == "-S";
+    let token_files = args
+        .iter()
+        .skip(if print_summary { 1 } else { 0 })
         .map(|x| Path::new(x).to_path_buf())
         .flat_map(extract_files)
         .flat_map(TokenFile::from)
-        .for_each(|f| {
-            println!("{}", f.name);
-            f.tokens
-                .iter()
-                .for_each(|t| println!("\t\t{}: {}", t.0, t.1));
-        });
+        .collect();
+
+    if print_summary {
+        print_token_summary(token_files);
+    } else {
+        print_token_files(token_files);
+    }
+}
+
+fn print_token_files(files: Vec<TokenFile>) {
+    files.iter().for_each(|f| {
+        println!("{}", f.name);
+        f.tokens
+            .iter()
+            .for_each(|t| println!("\t\t{}: {}", t.0, t.1));
+    });
+}
+
+fn print_token_summary(files: Vec<TokenFile>) {
+    files
+        .iter()
+        .flat_map(|f| &f.tokens)
+        .for_each(|f| println!("{}: {}", f.0, f.1));
 }
 
 fn extract_files(path: PathBuf) -> Vec<PathBuf> {
