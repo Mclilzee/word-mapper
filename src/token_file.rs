b5da4acd466051681740f885cac6b1ec1bf90ed3
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-17 05:59:22 +0100

    Finalize tokens counting in TokenFile

--- src/token_file.rs
+++ src/token_file.rs
@@ -1,18 +1,75 @@
-use std::{fs::read_to_string, os, path::PathBuf};
+use std::collections::HashMap;
+use std::{fs::read_to_string, path::PathBuf};
 
 pub struct TokenFile {
-    name: PathBuf,
+    name: String,
     tokens: Vec<(String, usize)>,
 }
 
 impl TokenFile {
-    fn from(path: PathBuf) -> Option<TokenFile> {
-        if let Ok(content) = read_to_string(&path) {
-            return Some(TokenFile {
-                name: path,
-                tokens: Vec::new(),
-            });
-        }
-        return None;
+    pub fn from(path: PathBuf) -> Option<TokenFile> {
+        let name = path.to_str().unwrap_or("Unknown").to_owned();
+        let content = read_to_string(&path);
+        if content.is_err() {
+            return None;
+        };
+
+        let tokens = extract_tokens(content.unwrap());
+        let tokens = count_tokens(tokens);
+
+        Some(TokenFile { name, tokens })
     }
+}
+
+fn extract_tokens(str: String) -> Vec<String> {
+    let chars: Vec<char> = str.chars().collect();
+
+    let mut tokens: Vec<String> = Vec::new();
+    let mut index = 0;
+
+    while index < chars.len() {
+        if chars[index].is_whitespace() {
+            index += 1;
+        } else if chars[index].is_numeric() {
+            let str: String = chars
+                .iter()
+                .skip(index)
+                .take_while(|c| c.is_numeric())
+                .collect();
+            index += str.len();
+            tokens.push(str);
+        } else if chars[index].is_alphabetic() {
+            let str: String = chars
+                .iter()
+                .skip(index)
+                .take_while(|c| c.is_alphanumeric())
+                .collect();
+            index += str.len();
+            tokens.push(str);
+        } else {
+            let str: String = chars.iter().skip(index).take(1).collect();
+            tokens.push(str);
+            index += 1;
+        }
+    }
+
+    tokens
+}
+
+fn count_tokens(tokens: Vec<String>) -> Vec<(String, usize)> {
+    let mut count: HashMap<String, usize> = HashMap::new();
+
+    for word in tokens {
+        let i = count.get(&word).unwrap_or(&0);
+        count.insert(word.to_owned(), i + 1);
+    }
+
+    let mut entries: Vec<(String, usize)> = count
+        .iter()
+        .map(|t| (t.0.to_owned(), t.1.to_owned()))
+        .collect();
+
+    entries.sort_by(|a, b| a.1.cmp(&b.1));
+
+    entries
 }
