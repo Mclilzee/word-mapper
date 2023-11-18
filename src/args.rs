44155d392275c00157cb7a839f1e1ae3d2e585fc
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-18 11:42:11 +0100

    Add clap configuration

---
+++ src/args.rs
@@ -0,0 +1,15 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[clap()]
+#[derive(Debug)]
+pub struct Args {
+    pub args: Vec<String>,
+
+    #[clap(short = 'C', long = "count")]
+    pub count: bool,
+
+    #[clap(short = 'S', long = "search")]
+    pub pwd: Option<Vec<&str>>,
+}
