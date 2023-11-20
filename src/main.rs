ffa43a8ba25131139976c243997034e35f38186c
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-20 17:06:39 +0100

    Add printing overall data

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use clap::Parser;
 use std::collections::HashMap;
 use std::process::exit;
 use std::{fs::read_dir, path::PathBuf};
-use token_file::TokenFile;
+use token_file::{Token, TokenFile};
 
 fn main() {
     let config = Args::parse();
@@ -38,7 +38,7 @@ fn main() {
     // };
 
     if config.overall {
-        // print_token_summary(token_files);
+        print_token_summary(token_files);
     } else {
         print_token_files(token_files);
     }
@@ -73,21 +73,33 @@ fn print_token_files(token_files: Vec<TokenFile>) {
     });
 }
 
-// fn print_token_summary(token_files: Vec<TokenFile>) {
-//     let mut sorted: Vec<(String, usize)> = token_files
-//         .iter()
-//         .map(|t| &t.tokens)
-//         .fold(HashMap::new(), |map, vec| {
-//             vec.iter().fold(map, |mut map, token| {
-//                 let count = map.get(&token).unwrap_or(&0) + token.occurence;
-//                 map.insert(token.to_owned(), count);
-//                 map
-//             })
-//         })
-//         .iter()
-//         .map(|(k, v)| (k.to_owned(), v.to_owned()))
-//         .collect();
-//
-//     sorted.sort_by(|a, b| a.1.cmp(&b.1));
-//     sorted.iter().for_each(|(t, c)| println!("{t}: {c}"));
-// }
+fn print_token_summary(token_files: Vec<TokenFile>) {
+    let total_occurences: usize = token_files
+        .iter()
+        .map(|t| &t.tokens)
+        .map(|t| t.iter().map(|t| t.occurence).sum::<usize>())
+        .sum();
+
+    let mut tokens: Vec<Token> = token_files
+        .iter()
+        .map(|t| &t.tokens)
+        .fold(HashMap::new(), |map, vec| {
+            vec.iter().fold(map, |mut map, token| {
+                let count = map.get(&token.symbol).unwrap_or(&0) + token.occurence;
+                map.insert(token.symbol.to_owned(), count);
+                map
+            })
+        })
+        .iter()
+        .map(|t| Token {
+            symbol: t.0.to_owned(),
+            occurence: t.1.to_owned(),
+            frequency: (t.1.to_owned() as f32 / total_occurences as f32) * 100.0,
+        })
+        .collect();
+
+    tokens.sort_by(|a, b| a.occurence.cmp(&b.occurence));
+    tokens
+        .iter()
+        .for_each(|t| println!("{}: {} ==== {:.3}%", t.symbol, t.occurence, t.frequency));
+}
